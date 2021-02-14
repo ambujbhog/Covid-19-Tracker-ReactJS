8d4d59f6a42e53b4fb21272b94b56ce9f23f6183
Ambuj Bhog <ambujbhog@protonmail.com>
2021-02-14 18:31:03 -0500

    installed Material CSS

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -4,6 +4,20 @@ function App() {
   return (
     <div className="app">
       <h1>COVID-19 Tracker</h1>
+
+      {/* Header */}
+      {/* Title + dropdown field */}
+
+      {/* Info box 1 */}
+      {/* Info box 2 */}
+      {/* Info box 3 */}
+
+      {/* Table */}
+
+      {/* Graph */}
+
+      {/* Map */}
+
      
     </div>
   );
