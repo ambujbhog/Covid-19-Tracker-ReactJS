a1cd3d0f5b2453a90c203cac79f5027069e6f153
Ambuj Bhog <ambujbhog@protonmail.com>
2021-02-17 14:58:03 -0500

    Data fetch and display from api

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -6,17 +6,25 @@ import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('worldwide');
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+
   useEffect(() => {
-    const getCountriesData = async () => {
-      await fetch ("https://disease.sh/v3/covid-19/countries")
+    fetch("https://disease.sh/v3/covid-19/all")
       .then((response) => response.json())
       .then((data) => {
-        const countries = data.map((country) => (
-          {
-            name: country.country, //United States, India
-            value: country.countryInfo.iso2 // Uk, USA, FR, IND
+        setCountryInfo(data);
+      });
+  }, []);                         
 
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
           }));
           setCountries(countries);
 
@@ -26,9 +34,20 @@ function App() {
 
   }, []);
 
-  const onCountryChange =  (event) => {
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    setCountry(countryCode);
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+
+    });
   };
 
 
@@ -38,29 +57,36 @@ function App() {
         <div className="app__header">
           <h1>COVID-19 Tracker</h1>
           <FormControl className="app_dropdown">
-          <Select variant="outlined"
-             onChange={onCountryChange}
-            value={country} >
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-
-            {countries.map((country) =>(
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}>
+              
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
                 <MenuItem value={country.value}>{country.name}</MenuItem>
               ))}
-          </Select>
+            </Select>
           </FormControl>
         </div>
 
         <div className="app__stats">
-           <InfoBox title="Coronavirus Cases" total={200} cases={12}/>
-           <InfoBox title="Recovered" total={200} cases={12}/>
-           <InfoBox title="Deaths" total={200} cases={12}/>
+           <InfoBox title="Coronavirus Cases"
+            cases={countryInfo.todayCases} 
+            total={countryInfo.cases}/>
+           <InfoBox title="Recovered" 
+            cases={countryInfo.todayRecovered} 
+            total={countryInfo.recovered}/>
+           <InfoBox title="Deaths" 
+            cases={countryInfo.todayDeaths} 
+            total={countryInfo.deaths}/>
         </div>
 
         <Map/>
 
       </div>
 
-      <Card className="app__right">
+      <Card className="app__right"> 
         <CardContent>
           <h3>Live Cases by Country</h3>
           {/* Table */}
